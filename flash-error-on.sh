137670d4cdf29adacad4d58f8840abfe998c7039
wtrabbit <34492316+wtrabbit@users.noreply.github.com>
2020-06-29 18:39:57 -0500

    Update flash-error-on.sh

--- flash-error-on.sh
+++ flash-error-on.sh
@@ -2,8 +2,8 @@
 
 while :
 do
-   sudo ./uhubctl/uhubctl -l 2 -a 0 > /dev/null 2>&1
+   sudo ~/uhubctl/uhubctl -l 2 -a 0 > /dev/null 2>&1
    sleep 1
-   sudo ./uhubctl/uhubctl -l 2 -a 1 > /dev/null 2>&1
+   sudo ~/uhubctl/uhubctl -l 2 -a 1 > /dev/null 2>&1
    sleep 1
 done
