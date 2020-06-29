9841205807a0950fed17e4f94bc5fc1255367321
wtrabbit <34492316+wtrabbit@users.noreply.github.com>
2020-06-29 16:59:30 -0500

    Update flash-error-on.sh

--- flash-error-on.sh
+++ flash-error-on.sh
@@ -3,8 +3,8 @@
 # run as "sudo ~/pihive-master/flash-error-on.sh &"
 while :
 do
-   sudo ~/pihive-master/uhubctl -l 2 -a 0 > /dev/null 2>&1
+   sudo ~/pihive-master/uhubctl/uhubctl -l 2 -a 0 > /dev/null 2>&1
    sleep 1
-   sudo ~/pihive-master/uhubctl -l 2 -a 1 > /dev/null 2>&1
+   sudo ~/pihive-master/uhubctl/uhubctl -l 2 -a 1 > /dev/null 2>&1
    sleep 1
 done
